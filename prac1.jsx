77dfba831450e31e8b0994dcdc3805139c409b75
Carl Erosa <carlmelvinerosa3@gmail.com>
2025-07-21 14:21:55 +0800

    Add initial implementation of myList component and rendering logic

---
+++ prac1.jsx
@@ -0,0 +1,11 @@
+const myList = (
+    <div>
+        <h1>Hello</h1>
+        <h1>World</h1>
+    </div>
+)
+
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(myList);
